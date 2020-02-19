11d08ab46bfd1aa7cfd1e076b44d33830e315632
Clemens Jaenicke <clemens.jaenicke@geo.hu-berlin.de>
2020-02-19 14:06:34 +0100

    Separated duplicate check and identifacation of intersecions

--- PreprocessingAndEditing/0003_repair.py
+++ PreprocessingAndEditing/0003_repair.py
@@ -1,3 +1,4 @@
+import time
 import os
 from osgeo import ogr
 
@@ -16,27 +17,6 @@ def createFolder(directory):
             os.makedirs(directory)
     except OSError:
         print ('Error: Creating directory. ' + directory)
-
-def removeLooseLines(in_shp_pth, dissolve):
-    negbuff_pth = in_shp_pth[:-4] + '_negbuffer.shp'
-    processing.run("native:buffer", {'INPUT': in_shp_pth,
-                                     'DISTANCE': -.1,
-                                     'SEGMENTS': 5,
-                                     'DISSOLVE': dissolve,
-                                     'END_CAP_STYLE': 2,
-                                     'JOIN_STYLE': 2,
-                                     'MITER_LIMIT': 2,
-                                     'OUTPUT': negbuff_pth})
-
-    posbuff_pth = in_shp_pth[:-4] + '_posbuffer.shp'
-    processing.run("native:buffer", {'INPUT': negbuff_pth,
-                                     'DISTANCE': .1,
-                                     'SEGMENTS': 5,
-                                     'DISSOLVE': dissolve,
-                                     'END_CAP_STYLE': 2,
-                                     'JOIN_STYLE': 1,
-                                     'MITER_LIMIT': 2,
-                                     'OUTPUT': posbuff_pth})
 
 def copyLayerToMemory(in_lyr):
     import ogr
@@ -64,7 +44,6 @@ def createEmptyShpWithCopiedLyr(in_lyr, out_pth, geom_type):
     :return: Output shapefile, output Layer
     """
     drv_shp = ogr.GetDriverByName('ESRI Shapefile')
-    # in_lyr = in_shp.GetLayer()
     in_sr = in_lyr.GetSpatialRef()
     in_lyr_defn = in_lyr.GetLayerDefn()
     if os.path.exists(out_pth):
@@ -77,13 +56,12 @@ def createEmptyShpWithCopiedLyr(in_lyr, out_pth, geom_type):
             lyr_out.CreateField(field_def)
     return shp_out, lyr_out
 
-def identifyIntersections(in_shp_pth, temp_folder):
-    file_name = os.path.basename(in_shp_pth)[:-4]
+def removeDuplicateFeatures(in_shp_pth, temp_folder, return_duplicates=False):
     createFolder(temp_folder)
+    file_name = os.path.basename(in_shp_pth)[:-4]
 
     in_shp = ogr.Open(in_shp_pth, 0)
     in_lyr = in_shp.GetLayer()
-    # in_sr = in_lyr.GetSpatialRef()
 
     copy_shp, copy_lyr = copyLayerToMemory(in_lyr)
 
@@ -91,151 +69,296 @@ def identifyIntersections(in_shp_pth, temp_folder):
     nodups_shp, nodups_lyr = createEmptyShpWithCopiedLyr(in_lyr=in_lyr, out_pth=nodups_shp_name,
                                                          geom_type=ogr.wkbPolygon)
     nodups_lyr_defn = nodups_lyr.GetLayerDefn()
-
-    inters_shp_name = temp_folder + r'\\' + file_name + '_intersection.shp'
-    inters_shp, inters_lyr = createEmptyShpWithCopiedLyr(in_lyr=in_lyr, out_pth=inters_shp_name,
-                                                         geom_type=ogr.wkbPolygon)
-    inters_lyr.CreateField(ogr.FieldDefn('IDInters', ogr.OFTString))
-    inters_lyr_defn = inters_lyr.GetLayerDefn()
-    num_fields = inters_lyr_defn.GetFieldCount()
-
     dupl_lst = []
-    covered_lst = []
-    id_inters_lst = []
-    for feat_curr in in_lyr:
+
+    if return_duplicates == True:
+        dups_shp_name = temp_folder + r'\\' + file_name + '_duplicates.shp'
+        dups_shp, dups_lyr = createEmptyShpWithCopiedLyr(in_lyr=in_lyr, out_pth=dups_shp_name,
+                                                         geom_type=ogr.wkbPolygon)
+        dups_lyr_defn = dups_lyr.GetLayerDefn()
+        dupl_lst = []
+
+    num_feat_total = in_lyr.GetFeatureCount()
+
+    for f, feat_curr in enumerate(in_lyr):
         id1 = feat_curr.GetField('ID')
         geom_curr = feat_curr.GetGeometryRef()
 
-        ## set a filter on the copied layer to
-        ## identify all features that might overlap
+        ## set a filter on the copied layer to identify all features that might overlap
         copy_lyr.SetSpatialFilter(geom_curr)
-
-        num_feat = copy_lyr.GetFeatureCount()
-        checker = 0
-        for feat_nb in copy_lyr:  # feat_nb = neighbouring feature
+        num_feat_nb = copy_lyr.GetFeatureCount() -1
+        progress = round((f / num_feat_total)*100, 2)
+        # print("Progress:", progress, "% ID:", id1, "Neighbouring features:", num_feat_nb )
+        dupl_check = 0
+        for feat_nb in copy_lyr:
             id2 = feat_nb.GetField('ID')
             id_inters = '{0}_{1}'.format(min([id1, id2]), max([id1, id2]))
-            print(id_inters)
             geom_nb = feat_nb.geometry()
 
-
-            if geom_nb.Intersects(geom_curr) and geom_nb != None:
-                intersection = geom_nb.Intersection(geom_curr)
-                geom_type = intersection.GetGeometryName()
-                if geom_type == 'MULTILINESTRING' or geom_type == 'POINT' or geom_type == 'LINESTRING':
-                    intersection = None
-            else:
-                intersection = None
-
-            if intersection == None or intersection.IsEmpty():
-                area_inters = 0
-                # print(id_inters, "is non-surface geometry type")
-            else:
-                area_inters = intersection.Area()
-
-            ## test for duplicates:
+            ## if both geoms are equal and IDs differ then increase duplicate check by 1
             if geom_curr.Equals(geom_nb) and id1 != id2:
                 dupl_lst.append(id_inters)
-                checker += 1
-                # print(id1, 'and', id2, 'are duplicates!')
+                dupl_check += 1
 
-            elif geom_nb.Within(geom_curr) and id1 != id2:
-                dupl_lst.append(id_inters)
-                covered_lst.append(id_inters)
-                checker += 1
-                # print('OGR Test:', id2, 'is within', id1)
-                # print(id2, area_nb, id1,  area_curr)
-
-            ## if the current feat of the copied layer is not a duplicate
-            ## and the neighbouring geom is not within the current geom
-            ## and the id of the intersection is not already in the list
-            ## then add this feature to the intersection layer
-            elif area_inters > 0 and id_inters not in id_inters_lst and id1 != id2:
-                intersection = intersection.Buffer(0)
-                intersection = intersection.MakeValid()
-                wkt_inters = intersection.ExportToWkt()
-                poly = ogr.CreateGeometryFromWkt(wkt_inters)
-                out_feat = ogr.Feature(inters_lyr_defn)
-                out_feat.SetGeometry(poly)
-                out_feat.SetField('ID', id1)
-                out_feat.SetField(num_fields - 1, id_inters)
-                inters_lyr.CreateFeature(out_feat)
-                ouf_feat = None
-
-                # print(id1, 'and', id2, 'intersect and are NOT (almost) duplicates!')
-                id_inters_lst.append(id_inters)
-
-        ## if the current feature is not a duplicate or
+        ## if the current feature is not a duplicate or w
         ## if it is a duplicate but the second version of it was net yet recorded in the duplicate list
         ## then add the current feature to the no duplicates layer
-        if checker == 0 or dupl_lst.count(id_inters) == 1:
+        if dupl_check == 0 or dupl_lst.count(id_inters) == 1:
             ouf_feat = ogr.Feature(nodups_lyr_defn)
             for i in range(0, nodups_lyr_defn.GetFieldCount()):
                 field_def = nodups_lyr_defn.GetFieldDefn(i)
                 field_name = field_def.GetName()
-                ouf_feat.SetField(nodups_lyr_defn.GetFieldDefn(i).GetNameRef(), feat_curr.GetField(i))
+                ouf_feat.SetField(field_name, feat_curr.GetField(i))
             geom_out = geom_curr.Clone()
             geom_out = geom_out.MakeValid()
             ouf_feat.SetGeometry(geom_out)
             nodups_lyr.CreateFeature(ouf_feat)
             ouf_feat = None
+        else:
+            if return_duplicates == True:
+                ouf_feat = ogr.Feature(dups_lyr_defn)
+                for i in range(0, dups_lyr_defn.GetFieldCount()):
+                    field_def = dups_lyr_defn.GetFieldDefn(i)
+                    field_name = field_def.GetName()
+                    ouf_feat.SetField(field_name, feat_curr.GetField(i))
+                geom_out = geom_curr.Clone()
+                geom_out = geom_out.MakeValid()
+                ouf_feat.SetGeometry(geom_out)
+                dups_lyr.CreateFeature(ouf_feat)
+                ouf_feat = None
+            else:
+                pass
 
         copy_lyr.SetSpatialFilter(None)
         copy_lyr.ResetReading()
     in_lyr.ResetReading()
     nodups_lyr.ResetReading()
+
+    del copy_shp, copy_lyr
+    del nodups_shp, nodups_lyr
+    del in_shp, in_lyr
+    if return_duplicates == True:
+        del dups_lyr, dups_shp
+
+def identifyIntersections(in_shp_pth, temp_folder):
+    file_name = os.path.basename(in_shp_pth)[:-4]
+    createFolder(temp_folder)
+
+    in_shp = ogr.Open(in_shp_pth, 0)
+    in_lyr = in_shp.GetLayer()
+
+    copy_shp, copy_lyr = copyLayerToMemory(in_lyr)
+
+    inters_shp_name = temp_folder + r'\\' + file_name + '_intersection.shp'
+    drv_shp = ogr.GetDriverByName('ESRI Shapefile')
+    in_sr = in_lyr.GetSpatialRef()
+    if os.path.exists(inters_shp_name):
+        drv_shp.DeleteDataSource(inters_shp_name)
+    inters_shp = drv_shp.CreateDataSource(inters_shp_name)
+    lyr_name = os.path.splitext(os.path.split(inters_shp_name)[1])[0]
+    geom_type = ogr.wkbPolygon
+    inters_lyr = inters_shp.CreateLayer(lyr_name, in_sr, geom_type=geom_type)
+    inters_lyr.CreateField(ogr.FieldDefn('ID', ogr.OFTInteger64))
+    inters_lyr.CreateField(ogr.FieldDefn('IDInters', ogr.OFTString))
+
+    inters_lyr_defn = inters_lyr.GetLayerDefn()
+    num_fields = inters_lyr_defn.GetFieldCount()
+
+    id_inters_lst = []
+    for feat_curr in in_lyr:
+        id1 = feat_curr.GetField('ID')
+        geom_curr = feat_curr.GetGeometryRef()
+        copy_lyr.SetSpatialFilter(geom_curr)
+
+        for feat_nb in copy_lyr:
+            id2 = feat_nb.GetField('ID')
+            id_inters = '{0}_{1}'.format(min([id1, id2]), max([id1, id2]))
+            geom_nb = feat_nb.geometry()
+            if id1 != id2:
+                if geom_nb.Intersects(geom_curr):
+                    intersection = geom_nb.Intersection(geom_curr)
+                    geom_type = intersection.GetGeometryName()
+                    if geom_type in ['MULTILINESTRING', 'POINT', 'LINESTRING','MULTIPOINT']: #alternatively mabye not in ['POLYGON', 'MULTIPOLYGON']
+                        intersection = None
+                        area_inters = 0
+                        # print("Intersection of {} and {} is a {}".format(id1, id2, geom_type))
+                    elif intersection != None:
+                        area_inters = round(intersection.Area(), 1)
+                        print("Intersection of {} and {} is NOT none. Its area is {} and its geom type is {}".format(id1, id2, area_inters, geom_type))
+                    else:
+                        intersection = None
+                        area_inters = 0
+                        # print("Intersection of {} and {} is none. Its area is {}".format(id1, id2, area_inters))
+                else:
+                    intersection = None
+                    area_inters = 0
+                    # print("{} and {} do not intersect.")
+
+                ## if the id of the intersection is not already in the list and its area is bigger than 0
+                ## then add this feature to the intersection layer
+                if area_inters > 0.0 and id_inters not in id_inters_lst:
+                    intersection = intersection.Buffer(0)
+                    intersection = intersection.MakeValid()
+                    wkt_inters = intersection.ExportToWkt()
+                    poly = ogr.CreateGeometryFromWkt(wkt_inters)
+                    out_feat = ogr.Feature(inters_lyr_defn)
+                    out_feat.SetGeometry(poly)
+                    out_feat.SetField('ID', id1)
+                    out_feat.SetField(num_fields - 1, id_inters)
+                    inters_lyr.CreateFeature(out_feat)
+                    ouf_feat = None
+
+                    id_inters_lst.append(id_inters)
+            else:
+                pass
+
+        copy_lyr.SetSpatialFilter(None)
+        copy_lyr.ResetReading()
+    in_lyr.ResetReading()
     inters_lyr.ResetReading()
 
     del copy_shp, copy_lyr
-    del in_shp, in_lyr
-    del nodups_shp, nodups_lyr
     del inters_shp, inters_lyr
+    del in_shp, in_lyr
+
+def removeLooseLines(in_shp_pth, dissolve):
+    negbuff_pth = in_shp_pth[:-4] + '_negbuffer.shp'
+    processing.run("native:buffer", {'INPUT': in_shp_pth,
+                                     'DISTANCE': -.1,
+                                     'SEGMENTS': 5,
+                                     'DISSOLVE': dissolve,
+                                     'END_CAP_STYLE': 2,
+                                     'JOIN_STYLE': 2,
+                                     'MITER_LIMIT': 2,
+                                     'OUTPUT': negbuff_pth})
+
+    posbuff_pth = in_shp_pth[:-4] + '_posbuffer.shp'
+    processing.run("native:buffer", {'INPUT': negbuff_pth,
+                                     'DISTANCE': .1,
+                                     'SEGMENTS': 5,
+                                     'DISSOLVE': dissolve,
+                                     'END_CAP_STYLE': 2,
+                                     'JOIN_STYLE': 1,
+                                     'MITER_LIMIT': 2,
+                                     'OUTPUT': posbuff_pth})
 
 wd = r'\\141.20.140.91\SAN_Projects\FORLand\Clemens\\'
 os.chdir(wd)
 
 ## INPUT
-in_shp_pth = r"Q:\FORLand\Clemens\data\vector\repairInvekos\sub_case\Antraege2008_sub.shp"
+in_shp_pth = r"Q:\FORLand\Clemens\data\vector\repairInvekos\complete_case\Antraege2008.shp"
+# in_shp_pth = r"Q:\FORLand\Clemens\data\vector\repairInvekos\sub_case2\Antraege2008_sub.shp"
 
 ## OUTPUT
-cleaned_pth = r"Q:\FORLand\Clemens\data\vector\repairInvekos\sub_case\Antraege2008_sub_cleaned.shp"
-
-## PROCESSING
-
+# cleaned_pth = r"Q:\FORLand\Clemens\data\vector\repairInvekos\sub_case2\Antraege2008_sub_cleaned.shp"
+cleaned_pth = r"Q:\FORLand\Clemens\data\vector\repairInvekos\complete_case\Antraege2008_cleaned.shp"
 temp_folder = in_shp_pth[:-4] + '_temp'
-identifyIntersections(in_shp_pth, temp_folder)
-
 file_name = os.path.basename(in_shp_pth)[:-4]
-nudups_pth = temp_folder + r'\\' + file_name + '_no_duplicates.shp'
-inters_pth = temp_folder + r'\\' + file_name + '_intersection.shp'
 
+logfile_pth = in_shp_pth[:-4] + '_logfile.txt'
+logfile = open(logfile_pth, "w+")
+
+# status_txt = "0. Remove duplicates from Invekos polygons"
+# print(status_txt)
+# tic = time.time()
+# removeDuplicateFeatures(in_shp_pth, temp_folder, return_duplicates=True)
+nodups_pth = temp_folder + r'\\' + file_name + '_no_duplicates.shp'
+# toc = time.time()
+# logfile.write(status_txt + ": " + str(22936) + " sec")
+# logfile.write(status_txt + ": " + str(toc-tic) + " sec")
+logfile.close()
+
+logfile = open(logfile_pth, "a+")
+status_txt = "\n1. Identify intersections of invekos polygons"
+print(status_txt)
+tic = time.time()
+identifyIntersections(nodups_pth, temp_folder)
+inters_pth = nodups_pth[:-4] + '_intersection.shp'
+toc = time.time()
+logfile.write(status_txt + ": " + str(toc-tic) + " sec")
+logfile.close()
+
+logfile = open(logfile_pth, "a+")
+status_txt = "\n2. Identify intersections of intersections"
+print(status_txt)
+tic = time.time()
 identifyIntersections(inters_pth, temp_folder)
 inters_inters_pth = inters_pth[:-4] + '_intersection.shp'
+toc = time.time()
+logfile.write(status_txt + ": " + str(toc-tic) + " sec")
+logfile.close()
 
-# createFolder(temp_folder)
-
+logfile = open(logfile_pth, "a+")
+status_txt ="\n3. Calc difference between intersections and intersections 2 "
+print(status_txt)
+tic = time.time()
 inters_wo_pth = temp_folder + r'\03_intersection_without_overlap.shp'
 param_dict = {'INPUT' : inters_pth,'OVERLAY' : inters_inters_pth,'OUTPUT' : inters_wo_pth}
 processing.run('native:difference', param_dict)
+toc = time.time()
+logfile.write(status_txt + ": " + str(toc-tic) + " sec")
+logfile.close()
 
+logfile = open(logfile_pth, "a+")
+status_txt = "\n4. Merge intersections"
+print(status_txt)
+tic = time.time()
 inters_merged_pth = temp_folder + r'\04_merged_intersections.shp'
 param_dict = {'LAYERS':[inters_inters_pth, inters_wo_pth], 'CRS':inters_inters_pth, 'OUTPUT': inters_merged_pth}
 processing.run('qgis:mergevectorlayers', param_dict)
+toc = time.time()
+logfile.write(status_txt + ": " + str(toc-tic) + " sec")
+logfile.close()
 
+logfile = open(logfile_pth, "a+")
+status_txt = "\n5. Remove loose lines of merged intersections"
+print(status_txt)
+tic = time.time()
 removeLooseLines(inters_merged_pth, False)
 inters_posbuff_pth = inters_merged_pth[:-4] + '_posbuffer.shp'
+toc = time.time()
+logfile.write(status_txt + ": " + str(toc-tic) + " sec")
+logfile.close()
 
+logfile = open(logfile_pth, "a+")
+status_txt = "\n6. Calc difference between invekos polygons and final intersections"
+print(status_txt)
+tic = time.time()
 poly_diff_pth = temp_folder + r'\05_difference_of_polygons.shp'
-param_dict = {'INPUT': nudups_pth,'OVERLAY': inters_posbuff_pth,'OUTPUT':poly_diff_pth}
+param_dict = {'INPUT': nodups_pth,'OVERLAY': inters_posbuff_pth,'OUTPUT':poly_diff_pth}
 processing.run('native:difference', param_dict)
+toc = time.time()
+logfile.write(status_txt + ": " + str(toc-tic) + " sec")
+logfile.close()
 
+logfile = open(logfile_pth, "a+")
+status_txt = "\n7. Remove loose lines of subtracted invekos polygons"
+print(status_txt)
+tic = time.time()
 removeLooseLines(poly_diff_pth, False)
 polydiff_posbuff_pth = poly_diff_pth[:-4] + '_posbuffer.shp'
+toc = time.time()
+logfile.write(status_txt + ": " + str(toc-tic) + " sec")
+logfile.close()
 
+logfile = open(logfile_pth, "a+")
+status_txt = "\n8. Merge subtracted polygons with final intersections"
+print(status_txt)
+tic = time.time()
 sliced_polys_pth = temp_folder + r'\06_sliced_polygons.shp'
 param_dict = {'LAYERS':[polydiff_posbuff_pth, inters_posbuff_pth], 'CRS':polydiff_posbuff_pth, 'OUTPUT': sliced_polys_pth}
 processing.run('qgis:mergevectorlayers', param_dict)
+toc = time.time()
+logfile.write(status_txt + ": " + str(toc-tic) + " sec")
+logfile.close()
 
+logfile = open(logfile_pth, "a+")
+status_txt = "\n9. Dissolve polygons and intersections based on ID"
+print(status_txt)
+tic = time.time()
 # dissolved_polys_pth = temp_folder + r'\09_dissolved_polygons.shp'
 param_dict = {'INPUT': sliced_polys_pth, 'FIELD':'ID', 'OUTPUT': cleaned_pth}
 processing.run('native:dissolve', param_dict)
+toc = time.time()
+logfile.write(status_txt + ": " + str(toc-tic) + " sec")
+logfile.close()
