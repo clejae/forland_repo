bf65b4b9dab1a2a07a7e604909e0c0dec09d5bfb
Clemens Jaenicke <clemens.jaenicke@geo.hu-berlin.de>
2020-03-17 17:38:16 +0100

    Uses geotools repo now

--- CropSequenceTypology/04_classify_struct_types.py
+++ CropSequenceTypology/04_classify_struct_types.py
@@ -7,51 +7,13 @@ import time
 import gdal
 import numpy as np
 import joblib
+
+## Clemens repo
+import raster
 # ------------------------------------------ DEFINE FUNCTIONS ------------------------------------------------#
 
-def writeArrayToRaster(in_array, out_path, gt, pr, no_data_value):
-
-    import gdal
-    from osgeo import gdal_array
-
-    type_code = gdal_array.NumericTypeCodeToGDALTypeCode(in_array.dtype)
-
-    if len(in_array.shape) == 3:
-        nbands_out = in_array.shape[0]
-        x_res = in_array.shape[2]
-        y_res = in_array.shape[1]
-
-        out_ras = gdal.GetDriverByName('GTiff').Create(out_path, x_res, y_res, nbands_out, gdal.GDT_Byte, options = [ 'COMPRESS=DEFLATE' ]) # type_code)
-        out_ras.SetGeoTransform(gt)
-        out_ras.SetProjection(pr)
-
-        for b in range(0, nbands_out):
-            band = out_ras.GetRasterBand(b + 1)
-            arr_out = in_array[b, :, :]
-            band.WriteArray(arr_out)
-            band.SetNoDataValue(no_data_value)
-            band.FlushCache()
-
-        del (out_ras)
-
-    if len(in_array.shape) == 2:
-        nbands_out = 1
-        x_res = in_array.shape[1]
-        y_res = in_array.shape[0]
-
-        out_ras = gdal.GetDriverByName('GTiff').Create(out_path, x_res, y_res, nbands_out, gdal.GDT_Byte, options = [ 'COMPRESS=DEFLATE' ]) # type_code
-        out_ras.SetGeoTransform(gt)
-        out_ras.SetProjection(pr)
-
-        band = out_ras.GetRasterBand( 1)
-        band.WriteArray(in_array)
-        band.SetNoDataValue(no_data_value)
-        band.FlushCache()
-
-        del (out_ras)
-
 # ------------------------------------------ GLOBAL VARIABLES ------------------------------------------------#
-wd = r'L:\Clemens\data\\'
+wd = r'\\141.20.140.91\SAN_Projects\FORLand\Clemens\data\\'
 
 cl_dict = {
         1: 0,
@@ -95,7 +57,7 @@ stime = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
 print("start: " + stime)
 os.chdir(wd)
 
-with open(r"L:\Clemens\data\raster\folder_list.txt") as file:
+with open(r"raster\folder_list.txt") as file:
     tiles_lst = file.readlines()
 tiles_lst = [item.strip() for item in tiles_lst]
 
@@ -115,7 +77,7 @@ for item in per_lst:
         ct_arr = np.full((7, 3000, 3000),255)
         for y, year in enumerate(range(min_year, max_year)):
 
-            ras_pth = r"L:\Clemens\data\raster\grid_15km\{0}\Inv_CropTypes_{1}_5m.tif".format(tile, year)
+            ras_pth = r"raster\grid_15km\{0}\Inv_CropTypes_{1}_5m.tif".format(tile, year)
             ras = gdal.Open(ras_pth)
 
             gt = ras.GetGeoTransform()
@@ -139,14 +101,14 @@ for item in per_lst:
             sw_arr[y, :, :] = out_arr2
 
 
-        pth = r"L:\Clemens\data\raster\grid_15km\{0}\{1}_Inv_CropTypes_5m.tif".format(tile, per)
-        writeArrayToRaster(ct_arr, pth, gt, pr, 255)
+        pth = r"raster\grid_15km\{0}\{1}_Inv_CropTypes_5m.tif".format(tile, per)
+        raster.writeArrayToRaster(ct_arr, pth, gt, pr, 255)
 
-        pth = r"L:\Clemens\data\raster\grid_15km\{0}\{1}_Inv_CerLeaf_5m.tif".format(tile, per)
-        writeArrayToRaster(cl_arr, pth, gt, pr, 255)
+        pth = r"raster\grid_15km\{0}\{1}_Inv_CerLeaf_5m.tif".format(tile, per)
+        raster.writeArrayToRaster(cl_arr, pth, gt, pr, 255)
 
-        pth = r"L:\Clemens\data\raster\grid_15km\{0}\{1}_Inv_WinSumm_5m.tif".format(tile, per)
-        writeArrayToRaster(sw_arr, pth, gt, pr, 255)
+        pth = r"raster\grid_15km\{0}\{1}_Inv_WinSumm_5m.tif".format(tile, per)
+        raster.writeArrayToRaster(sw_arr, pth, gt, pr, 255)
 
     if __name__ == '__main__':
         joblib.Parallel(n_jobs=4)(joblib.delayed(workFunc)(tile) for tile in tiles_lst)
