41a665fa927f0dd56d9b1c0dd775cba9ac3022ed
clejae <38685354+clejae@users.noreply.github.com>
2020-03-17 17:11:51 +0100

    forland_wrapper functions added

--- PreprocessingAndEditing/02a_clean_invekos.py
+++ PreprocessingAndEditing/02a_clean_invekos.py
@@ -1,167 +1,99 @@
+# Clemens Jänicke
+# github Repo: https://github.com/clejae
+
+# ------------------------------------------ LOAD PACKAGES ---------------------------------------------------#
 import time
 import os
 from osgeo import ogr
 
-## QGIS
-from qgis.core import QgsApplication, QgsProcessingRegistry
-from qgis.testing import start_app
-from qgis.analysis import QgsNativeAlgorithms
-import processing
-app = start_app()
-QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
-
 ## OWN REPOSITORY
 import general
 import vector
+import forland_wrapper
 
-def removeLooseLines(in_shp_pth, out_shp_pth, dissolve=False):
-    negbuff_pth = out_shp_pth[:-4] + '_negbuff.shp'
-    processing.run("native:buffer", {'INPUT': in_shp_pth,
-                                     'DISTANCE': -.1,
-                                     'SEGMENTS': 5,
-                                     'DISSOLVE': dissolve,
-                                     'END_CAP_STYLE': 2,
-                                     'JOIN_STYLE': 2,
-                                     'MITER_LIMIT': 2,
-                                     'OUTPUT': negbuff_pth})
+## QGIS
+QgsApplication, QgsProcessingRegistry, start_app,  QgsNativeAlgorithms, processing, app = vector.importQgis()
+QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
 
-    processing.run("native:buffer", {'INPUT': negbuff_pth,
-                                     'DISTANCE': .1,
-                                     'SEGMENTS': 5,
-                                     'DISSOLVE': dissolve,
-                                     'END_CAP_STYLE': 2,
-                                     'JOIN_STYLE': 1,
-                                     'MITER_LIMIT': 2,
-                                     'OUTPUT': out_shp_pth})
-
-######## Define wd, input & output folder
+# ------------------------------------------ START TIME ------------------------------------------------------#
+stime = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+print("start: " + stime)
+# ------------------------------------------ GLOBAL VARIABLES ------------------------------------------------#
 wd = r'\\141.20.140.91\SAN_Projects\FORLand\Clemens\\'
+# ------------------------------------------ LOAD DATA & PROCESSING ------------------------------------------#
 os.chdir(wd)
 
-in_shp_pth = r"data\vector\repairInvekos\complete_case\Antraege2009.shp"
-out_folder = r"data\vector\repairInvekos\complete_case\Antraege2009_temp\\"
-
-######## Processing
-file_name = os.path.basename(in_shp_pth)[:-4]
+## Input shapefile & output folder
+in_shp_pth = r"data\vector\repairInvekos\complete_case\Antraege2008.shp"
+out_folder = r"data\vector\repairInvekos\complete_case\Antraege2008_temp\\"
 general.createFolder(out_folder)
 
-#### Identify duplicate features
+#### Define paths
 no_dups_pth = out_folder + '01_noDuplicates.shp'
+
 inters_pth = out_folder + '02_intersections.shp'
-inters_inters_pth = out_folder + '03_intersections_of_intersections.shp'
-inters_wo_pth = out_folder + '04_intersection_without_overlap.shp'
-inters_merged_pth = out_folder + '05_merged_intersections.shp'
-inters_cleaned_pth = out_folder + '06_intersections_cleaned.shp'
-inters_cleaned_pth2 = out_folder + '06_intersections_cleaned_step2.shp'
-#polydiff_cleaned_pth2 = out_folder + '06_intersections_cleaned_step2.shp'
-poly_diff_pth = out_folder + '07_difference_polygons-intersections.shp'
-polydiff_cleaned_pth = out_folder + '08_difference_cleaned.shp'
-sliced_polys_pth = out_folder + r'\09_sliced_polygons.shp'
-cleaned_pth = in_shp_pth[:-4] + "_cleaned.shp"
+inters_sliced_path1 = out_folder + '02_intersections_sliced_01.shp'
+inters_sliced_path2 = out_folder + '02_intersections_sliced_02.shp'
+inters_sliced_path3 = out_folder + '02_intersections_sliced_03.shp'
 
-# status_txt = "1. Remove duplicates from Invekos polygons"
-# print(status_txt)
-#
-# id_lst, area_lst, centroid_lst = vector.extractGeomCharacteristics(in_shp_pth)
-#
-# ## look for identical areas and centroids
-# ## the area and centroid indices indicate the occurence of possible duplicates (only the second occurence)
-# area_duplicates, area_indices = general.findDuplicatesInList(area_lst)
-# centroid_duplicates, centroid_indices = general.findDuplicatesInList(centroid_lst)
-#
-# ## check for indices that indicate duplicates in areas and centroids
-# identical_indices = []
-# for index in area_indices:
-#     if index in centroid_indices:
-#         identical_indices.append(index)
-#
-# ## remove all second occurences from the total ID list
-# id_rem_lst = [id_lst[i] for i in identical_indices]
-# id_unique_lst = [item for item in id_lst if item not in id_rem_lst]
-#
-# ## write the features that are indicated by the cleaned ID list to a new shapefile
-# print("1.1 Writing cleaned shapefile")
-# in_shp = ogr.Open(in_shp_pth, 0)
-# in_lyr = in_shp.GetLayer()
-#
-# out_shp, out_lyr = vector.createEmptyShpWithCopiedLyr(in_lyr=in_lyr, out_pth=no_dups_pth, geom_type=ogr.wkbPolygon)
-#
-# if len(id_unique_lst) > 0:
-#     for id in id_unique_lst:
-#         feat = in_lyr.GetFeature(id)
-#         out_lyr.CreateFeature(feat)
-#     del out_shp, out_lyr
-# else:
-#     print("There are not duplicates in", in_shp_pth)
-########
+poly_diff_pth = out_folder + '03_difference.shp'
+polydiff_cleaned_pth1 = out_folder + '03_difference_cleaned_01.shp'
+polydiff_cleaned_pth2 = out_folder + '03_difference_cleaned_02.shp'
 
-# vector.validitiyCheck(no_dups_pth,"ID")
-# vector.makeGeomsValid(no_dups_pth)
-# vector.validitiyCheck(no_dups_pth,"ID")
+sliced_polys_pth1 = out_folder + r'\04_sliced_polygons_01.shp'
+sliced_polys_pth2 =  out_folder + r'\04_sliced_polygons_02.shp'
+sliced_polys_pth3 =  out_folder + r'\04_sliced_polygons_03.shp'
 
-# status_txt = "\n2. Identify intersections of invekos polygons"
-# print(status_txt)
-# vector.identifyIntersections(no_dups_pth, inters_pth)
-#
-# status_txt = "\n2.1 Make unvalid intersections valid"
-# print(status_txt)
-# vector.validitiyCheck(inters_pth, "IDInters")
-# vector.makeGeomsValid(inters_pth)
+cleaned_pth1 = out_folder + r"\05_nodups_cleaned_01.shp"
+cleaned_pth2 = out_folder + r"\05_nodups_cleaned_02.shp"
+cleaned_pth3 = out_folder + r"\05_nodups_cleaned_03.shp"
 
-# status_txt = "\n3. Identify intersections of intersections"
-# print(status_txt)
-# vector.identifyIntersections(inters_pth, inters_inters_pth)
-#
-# vector.validitiyCheck(inters_inters_pth, "ID")
-# vector.makeGeomsValid(inters_inters_pth)
+print("\nRemove duplicates from Invekos polygons and check and correct for validity of the polygons")
+forland_wrapper.removeDuplicates(in_shp_pth, no_dups_pth)
+forland_wrapper.validityChecking(no_dups_pth)
 
-# status_txt ="\n4. Calc difference between intersections and intersections 2 "
-# print(status_txt)
-# param_dict = {'INPUT' : inters_pth,'OVERLAY' : inters_inters_pth,'OUTPUT' : inters_wo_pth}
-# processing.run('native:difference', param_dict)
-#
-# status_txt = "\n5. Merge intersections"
-# print(status_txt)
-# param_dict = {'LAYERS':[inters_inters_pth, inters_wo_pth], 'CRS':inters_inters_pth, 'OUTPUT': inters_merged_pth}
-# processing.run('qgis:mergevectorlayers', param_dict)
-#
-# status_txt = "\n6. Remove loose lines of merged intersections"
-# print(status_txt)
-# removeLooseLines(inters_merged_pth, inters_cleaned_pth, False)
-# toc = time.time()
+print("\nIdentify intersections of no duplicates polygons and check and correct for validity of the intersections")
+vector.identifyIntersections(no_dups_pth, inters_pth)
+forland_wrapper.validityChecking(inters_pth)
 
-# status_txt = "\n6.1 Remove geometries that are none"
-# print(status_txt)
-# vector.removeNoneGeoms(inters_cleaned_pth, inters_cleaned_pth2)
+print("\nIdentify possible intersections and slice intersections")
+forland_wrapper.sliceIntersections(inters_pth, inters_sliced_path1)
+forland_wrapper.removeLooseLines(inters_sliced_path1, inters_sliced_path2, False)
+forland_wrapper.removingNoneGeoms(inters_sliced_path2, inters_sliced_path3)
+forland_wrapper.validityChecking(inters_sliced_path3)
 
-status_txt = "\n7. Calc difference between invekos polygons and final intersections"
-print(status_txt)
-param_dict = {'INPUT': no_dups_pth,'OVERLAY': inters_cleaned_pth,'OUTPUT':poly_diff_pth}
+print("\nCalc difference between invekos polygons and final intersections")
+param_dict = {'INPUT': no_dups_pth, 'OVERLAY': inters_pth, 'OUTPUT': poly_diff_pth}
 processing.run('native:difference', param_dict)
 
-vector.validitiyCheck(poly_diff_pth,'ID')
-#
-status_txt = "\n8. Remove loose lines of subtracted invekos polygons"
-print(status_txt)
-removeLooseLines(poly_diff_pth, polydiff_cleaned_pth, False)
+print("\nClean difference-polygons.")
+forland_wrapper.removeLooseLines(poly_diff_pth, polydiff_cleaned_pth1, False)
+forland_wrapper.removingNoneGeoms(polydiff_cleaned_pth1, polydiff_cleaned_pth2) ## --> no feature with null geoms
+forland_wrapper.validityChecking(polydiff_cleaned_pth2)
 
-vector.validitiyCheck(polydiff_cleaned_pth,'ID')
-#
-# #status_txt = "\n8.1 Remove geometries that are none"
-# #print(status_txt)
-#
-# #vector.removeNoneGeoms(polydiff_cleaned_pth, polydiff_cleaned_pth2)
-#
-# ## --> Use feat to write new shapefile instead of creating it from the geometries, see makeValid()
-#
-status_txt = "\n9. Merge subtracted polygons with final intersections"
-print(status_txt)
-param_dict = {'LAYERS':[polydiff_cleaned_pth, inters_cleaned_pth2], 'CRS':polydiff_cleaned_pth, 'OUTPUT': sliced_polys_pth}
+print("\nMerge difference-polygons with final intersections")
+param_dict = {'LAYERS':[polydiff_cleaned_pth2, inters_sliced_path3], 'CRS': polydiff_cleaned_pth2, 'OUTPUT': sliced_polys_pth1}
 processing.run('qgis:mergevectorlayers', param_dict)
 
-status_txt = "\n10. Dissolve polygons and intersections based on ID"
-print(status_txt)
-param_dict = {'INPUT': sliced_polys_pth, 'FIELD':'ID', 'OUTPUT': cleaned_pth}
+print("\nClean sliced polygons")
+forland_wrapper.removeLooseLines(sliced_polys_pth1, sliced_polys_pth2, False)
+forland_wrapper.removingNoneGeoms(sliced_polys_pth2, sliced_polys_pth3) #--> no features with no geometry in the shp
+forland_wrapper.validityChecking(sliced_polys_pth2)
+
+print("\nDissolve polygons and intersections based on ID")
+param_dict = {'INPUT': sliced_polys_pth2, 'FIELD':'ID', 'OUTPUT': cleaned_pth1}
 processing.run('native:dissolve', param_dict)
 
-print("Done!")
+print("\nRemove loose lines from inside of the polygons")
+forland_wrapper.removeLooseLines(cleaned_pth1, cleaned_pth2, False, dist = 0.01)
+
+print("\nConvert polygons to multipart where necessary")
+param_dict = {'INPUT': cleaned_pth2, 'OUTPUT': cleaned_pth3}
+processing.run('native:multiparttosingleparts', param_dict)
+forland_wrapper.validityChecking(cleaned_pth3)
+
+# ------------------------------------------ END TIME --------------------------------------------------------#
+etime = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+print("start: " + stime)
+print("end: " + etime)
+# ------------------------------------------ UNUSED BUT USEFUL CODE SNIPPETS ---------------------------------#
