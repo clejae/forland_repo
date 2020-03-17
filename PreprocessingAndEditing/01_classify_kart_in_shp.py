44601b01dbde811a80388089ddf16ef6609123b4
clejae <38685354+clejae@users.noreply.github.com>
2020-03-17 17:10:01 +0100

    ID_WiSo and ID_HaBl no longer derived from CropTyp

--- PreprocessingAndEditing/01_classify_kart_in_shp.py
+++ PreprocessingAndEditing/01_classify_kart_in_shp.py
@@ -6,69 +6,31 @@ import ogr
 import os
 import pandas as pd
 
-# ------------------------------------------ DEFINE FUNCTIONS ------------------------------------------------#
-def GetFieldNames(shp):
-    """
-    :param shp: Shapefile to get the field names from
-    :return: List of all field names
-    """
-    lyr = shp.GetLayer()
-    lyr_def = lyr.GetLayerDefn()
-
-    fname_lst = []
-    for i in range(lyr_def.GetFieldCount()):
-        fname = lyr_def.GetFieldDefn(i).GetName()
-        fname_lst.append(fname)
-
-    return fname_lst
+import vector
 # ------------------------------------------ GLOBAL VARIABLES ------------------------------------------------#
-wd = r'L:\Clemens\data\\'
+wd = r'\\141.20.140.91\SAN_Projects\FORLand\\'
 # ------------------------------------------ LOAD DATA & PROCESSING ------------------------------------------#
 os.chdir(wd)
 
 ## open reference table
-df_m = pd.read_excel(r"L:\Daten\vector\InVekos\Crops\Unique_crops_all_years_SteinSteinmann.xlsx", sheet_name='Sheet1')
-
-## fill NAs with value 80, which is the code for no data
-df_m['ID_KULTURTYP4_SandS'] = df_m['ID_KULTURTYP4_SandS'].fillna(80)
-df_m['ID_WinterSommer'] = df_m['ID_WinterSommer'].fillna(-9999)
-
-cl_dict = {
-    1:0,
-    2:0,
-    3:1,
-    4:1,
-    5:1,
-    6:0,
-    7:0,
-    9:0,
-    10:0,
-    12:0,
-    13:0,
-    14:0,
-    30:-9999,
-    60:1,
-    80:-9999,
-    99:-9999
-}
-
+df_m = pd.read_excel(r"Daten\vector\InVekos\Crops\Tables\UniqueCropCodes_AllYrsAndBundeslaender.xlsx", sheet_name='UniqueCodes')
 
 ## loop over shapefiles, add new column and fill it with code for the kulturtyp
-for year in range(2012,2019):
+for year in [2018]:
     print(year)
 
     ## open
-    shp_name = r"L:\Clemens\data\vector\misc\Inv_NoDups_{0}_testsub.shp".format(year)
+    shp_name = r"Clemens\data\vector\InvClassified\Inv_NoDups_{0}.shp".format(year)
     shp = ogr.Open(shp_name, 1)
     lyr = shp.GetLayer()
 
     ## get list of field names
-    fname_lst = GetFieldNames(shp)
+    fname_lst = vector.getFieldNames(shp)
 
     ## column name of Kulturtypen
-    fname_ktyp = "KULTURTYP"
-    fname_ws = "WintSumm"
-    fname_cl = "CerealLeaf"
+    fname_ktyp = "ID_KTYP"
+    fname_ws = "ID_WiSo"
+    fname_cl = "ID_HaBl"
 
     ## check if this column name already exists
     ## if yes, then no new column will be created
@@ -76,42 +38,45 @@ for year in range(2012,2019):
     if fname_ktyp in fname_lst:
         print("The field {0} exists already in the layer.".format(fname_ktyp))
     else:
-        lyr.CreateField(ogr.FieldDefn(fname_ktyp, ogr.OFTReal))
-        fname_lst = GetFieldNames(shp)
+        lyr.CreateField(ogr.FieldDefn(fname_ktyp, ogr.OFTInteger))
+        fname_lst = vector.getFieldNames(shp)
     if fname_ws in fname_lst:
         print("The field {0} exists already in the layer.".format(fname_ws))
     else:
-        lyr.CreateField(ogr.FieldDefn(fname_ws, ogr.OFTReal))
-        fname_lst = GetFieldNames(shp)
+        lyr.CreateField(ogr.FieldDefn(fname_ws, ogr.OFTInteger))
+        fname_lst = vector.getFieldNames(shp)
     if fname_cl in fname_lst:
         print("The field {0} exists already in the layer.".format(fname_cl))
     else:
-        lyr.CreateField(ogr.FieldDefn(fname_cl, ogr.OFTReal))
-        fname_lst = GetFieldNames(shp)
+        lyr.CreateField(ogr.FieldDefn(fname_cl, ogr.OFTInteger))
+        fname_lst = vector.getFieldNames(shp)
 
     ## loop over features and set kulturtyp and WinterSummer-code depending on the k_art code,
     ## set CerealLeaf-Code depending on kulturtyp
     for f, feat in enumerate(lyr):
+        fid = feat.GetField("ID")
 
         kart = feat.GetField('K_ART')
         kart = int(kart) # convert string to int
 
-        ktyp = df_m['ID_KULTURTYP4_SandS'].loc[df_m['K_ART_FL']==kart] # returns a pd Series
+        kart_k = feat.GetField('K_ART_K')
+
+        identifier = '{}_{}'.format(kart, kart_k)
+        print(year, fid, identifier)
+
+        ktyp = df_m['ID_KULTURTYP4_FL'].loc[df_m['K_ART_UNIQUE_noUmlaute']==identifier] # returns a pd Series
         ktyp = ktyp.iloc[0] # extracts value from pd Series
 
-        ws = df_m['ID_WinterSommer'].loc[df_m['K_ART_FL']==kart] # returns a pd Series
+        ws = df_m['ID_WinterSommer'].loc[df_m['K_ART_UNIQUE_noUmlaute']==identifier] # returns a pd Series
         ws = ws.iloc[0] # extracts value from pd Series
-        if ws == 13:
-            ws = 1
-        elif ws == 12:
-            ws = 0
-        else:
-            ws = -9999
 
-        cl = cl_dict[ktyp]
+        cl = df_m['ID_HalmfruchtBlattfrucht'].loc[df_m['K_ART_UNIQUE_noUmlaute']==identifier] # returns a pd Series
+        cl = cl.iloc[0]
 
-        feat.SetField(31, ktyp)
-        feat.SetField(32, ws)
-        feat.SetField(33, cl)
+        length = len(fname_lst)
+
+        feat.SetField(length-3, int(ktyp))
+        feat.SetField(length-2, int(ws))
+        feat.SetField(length-1, int(cl))
         lyr.SetFeature(feat)
     lyr.ResetReading()
