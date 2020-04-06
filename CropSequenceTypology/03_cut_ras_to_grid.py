e261b9781121cabfc8a2b30f08de7d81f67884e8
Clemens Jaenicke <clemens.jaenicke@geo.hu-berlin.de>
2020-04-06 12:01:19 +0200

    Update 06.04.2020

--- CropSequenceTypology/03_cut_ras_to_grid.py
+++ CropSequenceTypology/03_cut_ras_to_grid.py
@@ -83,3 +83,107 @@ print("end: " + etime)
 # ll = gdal.ApplyGeoTransform(inv_gt, x_min, y_min)
 # ur = gdal.ApplyGeoTransform(inv_gt, x_max, y_max)
 # lr = gdal.ApplyGeoTransform(inv_gt, x_max, y_min)
+
+################## VERSION 02 ##################
+# import os
+# import gdal
+# import ogr
+# import glob
+# import math
+# import osr
+#
+# ##CJ REPO
+# import vector
+#
+#
+# # ------------------------------------------ USER VARIABLES ------------------------------------------------#
+# wd = r'L:\Clemens\data\\'
+# # ------------------------------------------ LOAD DATA & PROCESSING ------------------------------------------#
+# os.chdir(wd)
+#
+# ras = gdal.Open(r"raster\2005-2005_Inv_Stack_5m_BIN_mosaic.tif")
+# gt = ras.GetGeoTransform()
+# inv_gt = gdal.InvGeoTransform(gt)
+#
+# grid_res = 15000
+#
+# ras_ext = vector.getCorners(r'raster\2005-2005_Inv_Stack_5m_BIN_mosaic.tif')
+#
+# x_ext = ras_ext[2] - ras_ext[0]
+# y_ext = ras_ext[3] - ras_ext[1]
+#
+# gt = ras.GetGeoTransform()
+# pr = ras.GetProjection()
+#
+# ras_res = gt[1]
+#
+# grid_col = math.ceil(x_ext / grid_res)
+# grid_row = math.ceil(y_ext / grid_res)
+#
+# # create shapefile
+# sr = osr.SpatialReference()
+# sr.ImportFromWkt(pr)
+# out_shp_name = 'L:/Clemens/data/vector/grid/Invekos_grid_15km.shp'
+# drv = ogr.GetDriverByName('ESRI Shapefile')
+# if os.path.exists(out_shp_name):
+#     drv.DeleteDataSource(out_shp_name)
+# lyr_name = 'Invekos_grid_01'
+# ds = drv.CreateDataSource(out_shp_name)
+# out_polygons = ds.CreateLayer(lyr_name, sr, ogr.wkbPolygon)
+# out_polygons.CreateField(ogr.FieldDefn("POLYID", ogr.OFTString))
+#
+# curr_xmin = ras_ext[0]
+# for col in range(0, grid_col):
+#     curr_ymin = ras_ext[1]
+#
+#     for row in range(0, grid_row):
+#         poly_id = 'X{0:02d}_Y{1:02d}'.format(col + 1, row + 1)
+#         print(poly_id)
+#
+#         x_min = curr_xmin
+#         x_max = curr_xmin + grid_res
+#         y_min = curr_ymin
+#         y_max = curr_ymin + grid_res
+#         print("   Xmin      -      Ymin      -      Xmax       -      Ymax")
+#         print(ras_ext[0],ras_ext[1],ras_ext[2],ras_ext[3])
+#         print(x_min, y_min, x_max, y_max, "\n")
+#
+#         out_path = r"raster\grid_15km\{0}\\".format(poly_id)
+#         vector.createFolder(out_path)
+#         out_name = out_path + '2005-2005_Inv_Stack_5m_BIN.tif'
+#
+#         # projWin --- subwindow in projected coordinates to extract: [ulx, uly, lrx, lry]
+#         ras_cut = gdal.Translate(out_name, ras, projWin=[x_min, y_max, x_max, y_min])# , projWinSRS=sr)
+#         ras_cut = None
+#
+#         ring = ogr.Geometry(ogr.wkbLinearRing)
+#         ring.AddPoint(curr_xmin, curr_ymin)
+#         ring.AddPoint(curr_xmin + grid_res, curr_ymin)
+#         ring.AddPoint(curr_xmin + grid_res, curr_ymin + grid_res)
+#         ring.AddPoint(curr_xmin, curr_ymin + grid_res)
+#         ring.AddPoint(curr_xmin, curr_ymin)
+#
+#         poly = ogr.Geometry(ogr.wkbPolygon)
+#         poly.AddGeometry(ring)
+#         poly.CloseRings()
+#
+#         out_defn = out_polygons.GetLayerDefn()  # get the layer definition
+#         out_feat = ogr.Feature(out_defn)  # erzeugt ein leeres dummy-feature
+#         out_feat.SetGeometry(poly)  # packt die polygone in das dummy feature
+#         out_polygons.CreateFeature(out_feat)  # fügt das feature zum layer hinzu
+#
+#         out_feat.SetField(0, poly_id)
+#         out_polygons.SetFeature(out_feat)
+#
+#         curr_ymin += grid_res
+#
+#     curr_xmin += grid_res
+#
+# ds = None
+#
+# # file_list = glob.glob(r'L:\Clemens\data\raster\grid_30km\**\2005-2005_Inv_Stack_5m_BIN.tif')
+# # vrt = gdal.BuildVRT(r'L:\Clemens\data\raster\grid_30km\2005-2005_Inv_Stack_5m_BIN.vrt', file_list)
+# # del(vrt)
+#
+# # print(gdal.Info(ras))
+# print("Script Done!")
--- StatisticalAnalysis/calculate_invekos_statistics_from_shp.py
+++ StatisticalAnalysis/calculate_invekos_statistics_from_shp.py
@@ -4,108 +4,166 @@
 # ------------------------------------------ LOAD PACKAGES ---------------------------------------------------#
 import os
 import ogr
-import gdal
+import joblib
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-# ------------------------------------------ DEFINE FUNCTIONS ------------------------------------------------#
-def printFieldNames(lyr):
-    lyr_defn = lyr.GetLayerDefn()
-    print("Column name | Field type | Width | Precision")
-    print("--------------------------------------------")
-    for i in range(lyr_defn.GetFieldCount()):
-        field_name = lyr_defn.GetFieldDefn(i).GetName()
-        field_type_code = lyr_defn.GetFieldDefn(i).GetType()
-        field_type = lyr_defn.GetFieldDefn(i).GetFieldTypeName(field_type_code)
-        field_width = lyr_defn.GetFieldDefn(i).GetWidth()
-        get_precision = lyr_defn.GetFieldDefn(i).GetPrecision()
-
-        print(field_name + " | " + field_type + " | " + str(field_width) + " | " + str(get_precision))
-# ------------------------------------------ GLOBAL VARIABLES ------------------------------------------------#
-wd = r'L:\Daten\vector\InVekos\Crops\AKTUELL_Invekos_20191217\\'
+import glob
+## CJ REPO
+import general
+# ------------------------------------------ USER VARIABLES ------------------------------------------------#
+# wd = r'\\141.20.140.91\SAN_Projects\FORLand\Daten\vector\InVekos\\'
+wd = r'\\141.20.140.91\SAN_Projects\FORLand\Clemens\data\\'
 # ------------------------------------------ LOAD DATA & PROCESSING ------------------------------------------#
 os.chdir(wd)
-df_lst = []
-area_lst = [["TotalArea","AlArea"]]
-for year in range(2005,2017):
-    print(year)
-    shp_pth = wd + r'Inv_NoDups_{0}.shp'.format(year)
-    shp = ogr.Open(shp_pth, 0) # 0=read only, 1=writeabel
-    lyr = shp.GetLayer()
 
-    printLayerColumns(lyr)
+bl_lst = ['BB', 'SA']
 
-    area_lst = []
-    type_lst = []
-    id_lst = []
-    for feat in lyr:
-        pt_id = feat.GetField('PtID')
-        area = feat.GetField('Area_H')
-        k_art_n = feat.GetField('K_ART_N')
+for bl in bl_lst:
 
-        id_lst.append(pt_id)
-        area_lst.append(area)
-        type_lst.append(k_art_n)
-    lyr.ResetReading()
+    if bl == 'BB':
+        # pth = r'Crops\AKTUELL_Invekos_20191217\\'
+        pth = r'vector\InvClassified\\'
+        file = 'Inv_NoDups_'
+        min_year = 2005
+        max_year = 2018
+    if bl == 'SA':
+        # pth = r'Sachsen-Anhalt\Shapefiles\AKTUELL_20200404\\'
+        pth = r'vector\InvClassified\\'
+        file = 'Antraege'
+        min_year = 2008
+        max_year = 2018
+    if bl == 'BY':
+        pth = r'Bayern\AKTUELL_20200604\\'
+        file = 'Nutzung'
+        min_year = 2005
+        max_year = 2019
 
-    df = pd.DataFrame()
-    df["K_ART_N"] = type_lst
-    df["Area_H"] = area_lst
-    df["ID"] = id_lst
+    year_lst = range(min_year, max_year + 1)
 
-    area_total = df["Area_H"].sum()
-    df_kult = df.groupby(['K_ART_N'])[['Area_H']].sum()#.unstack()
-    df_lst.append(df_kult)
+    def workFunc(year):
+    # for year in year_lst:
 
-    if year == 2016:
-        area_1 = df_kult.loc['AL',:][0]
-        area_2 = df_kult.loc['AI',:][0]
-        area_al = area_1 + area_2
-    else:
-        area_al = df_kult.loc['AL',:][0]
+        print(bl, year)
 
-    al_perc = area_al / area_total * 100
+        out_lst = [['BL','Year','FID','CountID','KTYP','Area']]
 
-    area_al_lst.append([area_total, area_al, al_perc])
+        shp_pth = pth + file + str(year) + '.shp'
+        shp = ogr.Open(shp_pth)
+        lyr = shp.GetLayer()
 
-area_lst = [["Year","TotalArea","AL-Area","AL-Perc"]]
-for i, df in enumerate(df_lst):
-    print(i + 2005)
-    # ind_names = df.index.values
-    # print(ind_names)
-    area_total = df["Area_H"].sum()
-    if i + 2005 == 2016:
-        area_1 = df.loc['AL',:][0]
-        area_2 = df.loc['AI',:][0]
-        area_al = area_1 + area_2
-    else:
-        area_al = df.loc['AL',:][0]
+        for f, feat in enumerate(lyr):
 
-    al_perc = round(area_al / area_total * 100,2)
+            fid = feat.GetField("ID")
+            ktyp = feat.GetField("ID_KTYP")
+            geom = feat.GetGeometryRef()
+            if geom != None:
+                area = geom.Area()
+            else:
+                area = -9999
 
-    area_lst.append([i + 2005, round(area_total,2), round(area_al,2), al_perc])
+            attr_lst = [bl, year, fid, f, ktyp, area]
+            out_lst.append(attr_lst)
 
-df = pd.DataFrame(area_lst)
-df.columns = df.iloc[0]
-df = df.drop(df.index[0])
-df.to_excel(r'L:\Clemens\data\tables\InVekos\k_art_k_area.xlsx', index=False)
+        out_pth = r'tables\InVekos\area_stats\area_stats_{}_{}.csv'.format(bl, year)
+        general.writeListToCSV(out_lst, out_pth)
 
+        print(bl, year, "done")
 
-plt.plot(range(2005,2017),area_al_lst)
-plt.bar(x= range(2005,2017), height=area_al_lst, width=0.8, align='center')
-plt.show()
-plt.close()
+    if __name__ == '__main__':
+        joblib.Parallel(n_jobs=14)(joblib.delayed(workFunc)(year) for year in year_lst)
 
-ras = gdal.Open(r"L:\Clemens\data\raster\grid\2005-2005_Inv_Stack_5m_BIN_mosaic.tif")
+pth_lst = glob.glob(r'tables\InVekos\area_stats\*.csv')
 
-with open(r"L:\Clemens\data\raster\grid\folder_list.txt") as file:
-    tiles_lst = file.readlines()
-tiles_lst = [item.strip() for item in tiles_lst]
+for csv_pth in pth_lst:
+    df = pd.read_csv(csv_pth)
+    df_lst.append(df)
 
-arr = ras.ReadAsArray()
-pix_count = np.sum(arr)
-area_m2 = pix_count * 25
-area_ha = area_m2 / (100 * 100)
-
-
+df = pd.concat(df_lst)
+df.to_csv(r'tables\InVekos\area_stats\area_stats_ALL_20200406.csv', index= False)
 # ------------------------------------------ UNUSED BUT USEFUL CODE SNIPPETS ---------------------------------#
+# import vector
+# import forland_wrapper
+# cleaned_pth2 = r"Q:\FORLand\Clemens\data\vector\InvClassified\Antraege2018.shp"
+# cleaned_pth3 = r"Q:\FORLand\Clemens\data\vector\InvClassified\Antraege2018_v02.shp"
+# forland_wrapper.removingNoneGeoms(cleaned_pth2, cleaned_pth3)
+
+# df_lst = []
+# area_lst = [["TotalArea","AlArea"]]
+# for year in range(2005,2017):
+#     print(year)
+#     shp_pth = wd + r'Inv_NoDups_{0}.shp'.format(year)
+#     shp = ogr.Open(shp_pth, 0) # 0=read only, 1=writeabel
+#     lyr = shp.GetLayer()
+#
+#     printLayerColumns(lyr)
+#
+#     area_lst = []
+#     type_lst = []
+#     id_lst = []
+#     for feat in lyr:
+#         pt_id = feat.GetField('PtID')
+#         area = feat.GetField('Area_H')
+#         k_art_n = feat.GetField('K_ART_N')
+#
+#         id_lst.append(pt_id)
+#         area_lst.append(area)
+#         type_lst.append(k_art_n)
+#     lyr.ResetReading()
+#
+#     df = pd.DataFrame()
+#     df["K_ART_N"] = type_lst
+#     df["Area_H"] = area_lst
+#     df["ID"] = id_lst
+#
+#     area_total = df["Area_H"].sum()
+#     df_kult = df.groupby(['K_ART_N'])[['Area_H']].sum()#.unstack()
+#     df_lst.append(df_kult)
+#
+#     if year == 2016:
+#         area_1 = df_kult.loc['AL',:][0]
+#         area_2 = df_kult.loc['AI',:][0]
+#         area_al = area_1 + area_2
+#     else:
+#         area_al = df_kult.loc['AL',:][0]
+#
+#     al_perc = area_al / area_total * 100
+#
+#     area_al_lst.append([area_total, area_al, al_perc])
+#
+# area_lst = [["Year","TotalArea","AL-Area","AL-Perc"]]
+# for i, df in enumerate(df_lst):
+#     print(i + 2005)
+#     # ind_names = df.index.values
+#     # print(ind_names)
+#     area_total = df["Area_H"].sum()
+#     if i + 2005 == 2016:
+#         area_1 = df.loc['AL',:][0]
+#         area_2 = df.loc['AI',:][0]
+#         area_al = area_1 + area_2
+#     else:
+#         area_al = df.loc['AL',:][0]
+#
+#     al_perc = round(area_al / area_total * 100,2)
+#
+#     area_lst.append([i + 2005, round(area_total,2), round(area_al,2), al_perc])
+#
+# df = pd.DataFrame(area_lst)
+# df.columns = df.iloc[0]
+# df = df.drop(df.index[0])
+# df.to_excel(r'L:\Clemens\data\tables\InVekos\k_art_k_area.xlsx', index=False)
+#
+#
+# plt.plot(range(2005,2017),area_al_lst)
+# plt.bar(x= range(2005,2017), height=area_al_lst, width=0.8, align='center')
+# plt.show()
+# plt.close()
+#
+# ras = gdal.Open(r"L:\Clemens\data\raster\grid\2005-2005_Inv_Stack_5m_BIN_mosaic.tif")
+#
+# with open(r"L:\Clemens\data\raster\grid\folder_list.txt") as file:
+#     tiles_lst = file.readlines()
+# tiles_lst = [item.strip() for item in tiles_lst]
+#
+# arr = ras.ReadAsArray()
+# pix_count = np.sum(arr)
+# area_m2 = pix_count * 25
+# area_ha = area_m2 / (100 * 100)
