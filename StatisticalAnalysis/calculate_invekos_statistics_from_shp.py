a6c77937ff59f7890e7afa116e45e73c16f88fd5
clejae <jaenikec@hu-berlin.de>
2020-04-07 12:13:27 +0200

    Update 07.04.2020

--- StatisticalAnalysis/calculate_invekos_statistics_from_shp.py
+++ StatisticalAnalysis/calculate_invekos_statistics_from_shp.py
@@ -11,74 +11,156 @@ import glob
 import general
 # ------------------------------------------ USER VARIABLES ------------------------------------------------#
 # wd = r'\\141.20.140.91\SAN_Projects\FORLand\Daten\vector\InVekos\\'
-wd = r'\\141.20.140.91\SAN_Projects\FORLand\Clemens\data\\'
+# wd = r'\\141.20.140.91\SAN_Projects\FORLand\Clemens\data\\'
+wd = r'C:\Users\Clemens Jänicke\Desktop\FORLAND\data\\'
 # ------------------------------------------ LOAD DATA & PROCESSING ------------------------------------------#
 os.chdir(wd)
 
 bl_lst = ['BB', 'SA']
+#
+# for bl in bl_lst:
+#
+#     if bl == 'BB':
+#         # pth = r'Crops\AKTUELL_Invekos_20191217\\'
+#         pth = r'vector\InvClassified\\'
+#         file = 'Inv_NoDups_'
+#         min_year = 2005
+#         max_year = 2018
+#     if bl == 'SA':
+#         # pth = r'Sachsen-Anhalt\Shapefiles\AKTUELL_20200404\\'
+#         pth = r'vector\InvClassified\\'
+#         file = 'Antraege'
+#         min_year = 2008
+#         max_year = 2018
+#     if bl == 'BY':
+#         pth = r'Bayern\AKTUELL_20200604\\'
+#         file = 'Nutzung'
+#         min_year = 2005
+#         max_year = 2019
+#
+#     year_lst = range(min_year, max_year + 1)
+#
+#     def workFunc(year):
+#     # for year in year_lst:
+#
+#         print(bl, year)
+#
+#         out_lst = [['BL','Year','FID','CountID','KTYP','Area']]
+#
+#         shp_pth = pth + file + str(year) + '.shp'
+#         shp = ogr.Open(shp_pth)
+#         lyr = shp.GetLayer()
+#
+#         for f, feat in enumerate(lyr):
+#
+#             fid = feat.GetField("ID")
+#             ktyp = feat.GetField("ID_KTYP")
+#             geom = feat.GetGeometryRef()
+#             if geom != None:
+#                 area = geom.Area()
+#             else:
+#                 area = -9999
+#
+#             attr_lst = [bl, year, fid, f, ktyp, area]
+#             out_lst.append(attr_lst)
+#
+#         out_pth = r'tables\InVekos\area_stats\area_stats_{}_{}.csv'.format(bl, year)
+#         general.writeListToCSV(out_lst, out_pth)
+#
+#         print(bl, year, "done")
+#
+#     if __name__ == '__main__':
+#         joblib.Parallel(n_jobs=14)(joblib.delayed(workFunc)(year) for year in year_lst)
+#
+# pth_lst = glob.glob(r'tables\InVekos\area_stats\*.csv')
+#
+# for csv_pth in pth_lst:
+#     df = pd.read_csv(csv_pth)
+#     df_lst.append(df)
+#
+# df = pd.concat(df_lst)
+# df.to_csv(r'tables\InVekos\area_stats\area_stats_ALL_20200406.csv', index= False)
+df = pd.read_csv(r"tables\InVekos\area_stats\area_stats_ALL_20200406.csv")
 
+## Change data types where necessary
+df.info()
+df['BL'] = df['BL'].astype('str')
+df['Area'] = df['Area'].astype('float')
+
+## Calculate statistics
+## first convert sqm to ha
+df['Area'] = df['Area'].div(10000)
+
+df_lst = []
 for bl in bl_lst:
+    df_sub = df[df.BL == bl]
 
-    if bl == 'BB':
-        # pth = r'Crops\AKTUELL_Invekos_20191217\\'
-        pth = r'vector\InvClassified\\'
-        file = 'Inv_NoDups_'
-        min_year = 2005
-        max_year = 2018
-    if bl == 'SA':
-        # pth = r'Sachsen-Anhalt\Shapefiles\AKTUELL_20200404\\'
-        pth = r'vector\InvClassified\\'
-        file = 'Antraege'
-        min_year = 2008
-        max_year = 2018
-    if bl == 'BY':
-        pth = r'Bayern\AKTUELL_20200604\\'
-        file = 'Nutzung'
-        min_year = 2005
-        max_year = 2019
+    df1 = df_sub.groupby(['Year'])[['Area']].max()
+    df1.columns = ['Max Area']
+    df2 = df_sub.groupby(['Year'])[['Area']].min()
+    df2.columns = ['Min Area']
+    df3 = df_sub.groupby(['Year'])[['Area']].mean()
+    df3.columns = ['Mean Area']
+    df4 = df_sub.groupby(['Year'])[['Area']].sum()
+    df4.columns = ['Sum Area']
+    df5 = df_sub.groupby(['Year']).size()
+    df5 = pd.DataFrame(df5)
+    df5.columns = ['Count']
 
-    year_lst = range(min_year, max_year + 1)
+    df_summ = pd.concat([df1, df2, df3, df4, df5], 1)
+    df_lst.append(df_summ)
 
-    def workFunc(year):
-    # for year in year_lst:
+df_lst2 = []
+ct_lst = df['KTYP'].unique()
+ct_lst.sort()
+for bl in bl_lst:
+    df_sub = df[df.BL == bl]
 
-        print(bl, year)
+    df1 = df_sub.groupby(['Year','KTYP'])[['Area']].max().unstack()
+    df1.columns = ['Max Area' + str(i) for i in ct_lst]
+    df2 = df_sub.groupby(['Year','KTYP'])[['Area']].min().unstack()
+    df2.columns = ['Min Area' + str(i) for i in ct_lst]
+    df3 = df_sub.groupby(['Year','KTYP'])[['Area']].mean().unstack()
+    df3.columns = ['Mean Area' + str(i) for i in ct_lst]
+    df4 = df_sub.groupby(['Year','KTYP'])[['Area']].sum().unstack()
+    df4.columns = ['Sum Area' + str(i) for i in ct_lst]
+    df5 = df_sub.groupby(['Year','KTYP']).size().unstack()
+    df5.columns = ['Count' + str(i) for i in ct_lst]
 
-        out_lst = [['BL','Year','FID','CountID','KTYP','Area']]
+    df_summ = pd.concat([df1, df2, df3, df4, df5],1)
+    df_lst2.append(df_summ)
 
-        shp_pth = pth + file + str(year) + '.shp'
-        shp = ogr.Open(shp_pth)
-        lyr = shp.GetLayer()
+df_lst3 = []
+for bl in bl_lst:
+    df_sub = df[df.BL == bl]
+    df_sub = df_sub[(df_sub.KTYP != 30) & (df_sub.KTYP != 80) & (df_sub.KTYP != 99)]
 
-        for f, feat in enumerate(lyr):
+    df1 = df_sub.groupby(['Year'])[['Area']].max()
+    df1.columns = ['Max Area']
+    df2 = df_sub.groupby(['Year'])[['Area']].min()
+    df2.columns = ['Min Area']
+    df3 = df_sub.groupby(['Year'])[['Area']].mean()
+    df3.columns = ['Mean Area']
+    df4 = df_sub.groupby(['Year'])[['Area']].sum()
+    df4.columns = ['Sum Area']
+    df5 = df_sub.groupby(['Year'])[['CountID']].size()
+    df5.columns = ['Count']
 
-            fid = feat.GetField("ID")
-            ktyp = feat.GetField("ID_KTYP")
-            geom = feat.GetGeometryRef()
-            if geom != None:
-                area = geom.Area()
-            else:
-                area = -9999
+    df_summ = pd.concat([df1, df2, df3, df4, df5],1)
+    df_lst3.append(df_summ)
 
-            attr_lst = [bl, year, fid, f, ktyp, area]
-            out_lst.append(attr_lst)
+with pd.ExcelWriter(r'tables\InVekos\area_stats\area_statistics_ALL.xlsx') as writer:
+    for d, df_out in enumerate(df_lst):
+        bl = bl_lst[d]
+        df_out.to_excel(writer, sheet_name = bl + 'TotalStatistics')
+    for d, df_out in enumerate(df_lst2):
+        bl = bl_lst[d]
+        df_out.to_excel(writer, sheet_name=bl + 'CropTypeStatistics')
+    for d, df_out in enumerate(df_lst3):
+        bl = bl_lst[d]
+        df_out.to_excel(writer, sheet_name=bl + 'TotalALStatistics')
 
-        out_pth = r'tables\InVekos\area_stats\area_stats_{}_{}.csv'.format(bl, year)
-        general.writeListToCSV(out_lst, out_pth)
 
-        print(bl, year, "done")
-
-    if __name__ == '__main__':
-        joblib.Parallel(n_jobs=14)(joblib.delayed(workFunc)(year) for year in year_lst)
-
-pth_lst = glob.glob(r'tables\InVekos\area_stats\*.csv')
-
-for csv_pth in pth_lst:
-    df = pd.read_csv(csv_pth)
-    df_lst.append(df)
-
-df = pd.concat(df_lst)
-df.to_csv(r'tables\InVekos\area_stats\area_stats_ALL_20200406.csv', index= False)
 # ------------------------------------------ UNUSED BUT USEFUL CODE SNIPPETS ---------------------------------#
 # import vector
 # import forland_wrapper
